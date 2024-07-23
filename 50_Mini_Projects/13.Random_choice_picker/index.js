6d263624eb2eaee4cc7c3e4523dca4641521dcc3
Dipesh-sapkota1 <sapkotadipes.com>
2024-07-23 12:09:47 +0545

    random_choice_picker

--- 50_Mini_Projects/13.Random_choice_picker/index.js
+++ 50_Mini_Projects/13.Random_choice_picker/index.js
@@ -1,1 +1,64 @@
+ const textarea  = document.getElementById('textarea');
+ const tagsEl = document.getElementById('tags');
+
+ textarea.focus();
  
+ textarea.addEventListener('keyup',(e)=>{
+    createTags(e.target.value);
+    if(e.key === 'Enter'){
+        setTimeout(() => {
+            e.target.value = '';
+        }, 10);
+        select_Random();
+    }
+ });
+
+function select_Random() {
+    const times = 30;
+    const interval =  setInterval(() => {
+        const randomTag = get_Randomtag();
+        add_Highlight(randomTag);
+        setTimeout(() => {
+            remove_Highlight(randomTag);
+        }, 100);
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval);
+
+        setTimeout(() => {
+            const randomTag = get_Randomtag();
+            add_Highlight(randomTag);
+        }, 100);
+    }, times * 100);
+}
+
+
+
+ function createTags(input) {
+    const tags = input.split(",")
+    .filter(tag => tag.trim() !== '')
+    .map(tag=> tag.trim());
+    
+    tagsEl.innerHTML = '';
+
+  tags.forEach(tag => {
+    const tagEl = document.createElement('span');
+    tagEl.classList.add('tag');
+    tagEl.innerText = tag;
+    tagsEl.appendChild(tagEl);
+  });
+   
+ }
+ 
+
+ function get_Randomtag(){
+    const tags = document.querySelectorAll('.tag');
+    return tags[Math.floor(Math.random() * tags.length)];
+ }
+ function add_Highlight(input){
+    input.classList.add('highlight');
+ }
+ function remove_Highlight(input){
+    input.classList.remove('highlight');
+ }
